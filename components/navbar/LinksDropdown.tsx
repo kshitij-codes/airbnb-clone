0cbd34196a1b86796098a72b15a7c4dcae062fb1
Kshitij Sharma <kshitijsharma@Kshitijs-MacBook-Air.local>
2024-08-31 20:23:52 +0530

    dark mode implemented

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "../ui/dropdown-menu";
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { Button } from "../ui/button";
+import { LuAlignLeft } from "react-icons/lu";
+import UserIcon from "./UserIcon";
+import { links } from "@/utils/links";
+import Link from "next/link";
 
 const LinksDropdown = () => {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex gap-4 max-w-[100px]">
+          <LuAlignLeft className="h-6 w-6 " />
+          <UserIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-52" align="start" sideOffset={10}>
+        {links.map((link) => (
+          <DropdownMenuItem key={link.href}>
+            <Link href={link.href} className="capitalize w-full">
+              {link.label}
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default LinksDropdown;
